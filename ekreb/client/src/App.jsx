6bcb3a70d52a63f51cb1339783c6ad7610ea11b7
Tristan <vantristan04@gmail.com>
2023-09-13 11:50:24 -0500

    create separate folders for frontend and backend

--- ekreb/client/src/App.jsx
+++ ekreb/client/src/App.jsx
@@ -2,11 +2,10 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
   const [guess, setGuess] = useState("")
 
   const handleGuess = () => {
-    
+
   }
   return (
     <>
@@ -15,14 +14,15 @@ function App() {
        <h2>Guess the scrambled word!</h2>
       </div>
 
-      <div className="game"></div>
-      <input
-          type="string"
-          placeholder="Enter your guess here!"
-          value={guess}
-          onChange={(e) => setGuess(e.target.value)}
-        />
-      <button onClick={handleGuess}>Guess</button>
+      <div className="game">
+        <input
+            type="string"
+            placeholder="Enter your guess here!"
+            value={guess}
+            onChange={(e) => setGuess(e.target.value)}
+          />
+        <button onClick={handleGuess}>Guess</button>
+      </div>
     </>
   )
 }
