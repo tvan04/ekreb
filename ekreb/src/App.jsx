81da6caffb44e0bb39b5ad34a3329f2278b897dd
Tristan <vantristan04@gmail.com>
2023-09-13 11:32:11 -0500

    create heading and input

--- ekreb/src/App.jsx
+++ ekreb/src/App.jsx
@@ -1,33 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [guess, setGuess] = useState("")
 
+  const handleGuess = () => {
+    
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="heading">
+       <h1>ekreb</h1>
+       <h2>Guess the scrambled word!</h2>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <div className="game"></div>
+      <input
+          type="string"
+          placeholder="Enter your guess here!"
+          value={guess}
+          onChange={(e) => setGuess(e.target.value)}
+        />
+      <button onClick={handleGuess}>Guess</button>
     </>
   )
 }
