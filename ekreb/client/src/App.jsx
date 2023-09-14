e024b57144f4b236766cc89f76cef000fc0061fc
Tristan <vantristan04@gmail.com>
2023-09-14 14:36:50 -0500

    write code for sending guesses to backend

--- ekreb/client/src/App.jsx
+++ ekreb/client/src/App.jsx
@@ -1,30 +1,41 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  const [guess, setGuess] = useState("")
+  const [guess, setGuess] = useState("");
+  const [word, setWord] = useState("");
 
-  const handleGuess = () => {
-
-  }
+  const handleGuess = async () => {
+    try {
+      const response = await axios.post("/api/ekreb/validate", {
+        userGuess: guess,
+        originalWord: word,
+      });
+      }
+    catch {
+      console.log("error");
+    }
+};
   return (
     <>
       <div className="heading">
-       <h1>ekreb</h1>
-       <h2>Guess the scrambled word!</h2>
+        <h1>ekreb</h1>
+        <h2>Guess the scrambled word!</h2>
       </div>
 
       <div className="game">
+      <p>Word: {word}</p>
         <input
-            type="string"
-            placeholder="Enter your guess here!"
-            value={guess}
-            onChange={(e) => setGuess(e.target.value)}
-          />
+          type="string"
+          placeholder="Enter your guess here!"
+          value={guess}
+          onChange={(e) => setGuess(e.target.value)}
+        />
         <button onClick={handleGuess}>Guess</button>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
