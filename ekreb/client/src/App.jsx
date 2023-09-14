a29266a76d14aad61845a8a6219d98c3775c820a
Tristan <vantristan04@gmail.com>
2023-09-14 18:50:43 -0500

    implemented score and fixed handleGuess function

--- ekreb/client/src/App.jsx
+++ ekreb/client/src/App.jsx
@@ -1,22 +1,46 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
 
 function App() {
   const [guess, setGuess] = useState("");
   const [word, setWord] = useState("");
+  const [score, setScore] = useState(0);
 
   const handleGuess = async () => {
     try {
-      const response = await axios.post("/api/ekreb/validate", {
-        userGuess: guess,
-        originalWord: word,
-      });
-      }
-    catch {
+      const response = await axios.post(
+        "http://localhost:3000/api/ekreb/validate",
+        {
+          guess: guess,
+
+        }
+      );
+    } catch {
       console.log("error");
     }
-};
+    const response = await axios.get("http://localhost:3000/api/ekreb/score");
+    setScore(response.data.score);
+    
+  };
+
+  useEffect(() => {
+    const fetchWord = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:3000/api/ekreb/random-word"
+        );
+        if (response.data) {
+          setWord(response.data.word);
+        }
+      } catch (error) {
+        console.error("Error fetching word:", error);
+      }
+    };
+
+    fetchWord();
+  }, []);
+
   return (
     <>
       <div className="heading">
@@ -25,7 +49,7 @@ function App() {
       </div>
 
       <div className="game">
-      <p>Word: {word}</p>
+        <p>Word: {word}</p>
         <input
           type="string"
           placeholder="Enter your guess here!"
@@ -33,6 +57,7 @@ function App() {
           onChange={(e) => setGuess(e.target.value)}
         />
         <button onClick={handleGuess}>Guess</button>
+        <p>Score: {score}</p>
       </div>
     </>
   );
