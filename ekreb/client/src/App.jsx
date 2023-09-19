b9ef019dd95e438347b6190b3e4360e9caa0e71e
Tristan <vantristan04@gmail.com>
2023-09-18 23:49:25 -0500

    add states for start, end, game, and result

--- ekreb/client/src/App.jsx
+++ ekreb/client/src/App.jsx
@@ -4,52 +4,90 @@ import axios from "axios";
 
 function App() {
   const [guess, setGuess] = useState("");
-  const [word, setWord] = useState("");
+  const [originalWord, setOriginalWord] = useState("");
+  const [scrambledWord, setScrambledWord] = useState("");
   const [score, setScore] = useState(0);
+  const [correct, setCorrect] = useState(false);
+  const [gameState, setGameState] = useState("start");
+  const [wordsPlayed, setWordsPlayed] = useState(0);
 
+  //checks to see how many words have been played and ends game after 5
+  useEffect(() => {
+    if (wordsPlayed == 5) {
+      setGameState("end");
+    }
+  }, [wordsPlayed]);
+
+  //sends guess to sever for validation, clears guess, fetches new word, increments words played
   const handleGuess = async () => {
     try {
-      const response = await axios.post(
-        "http://localhost:3000/api/ekreb/validate",
-        {
-          guess: guess,
-
-        }
-      );
+      await axios.post("http://localhost:3000/api/ekreb/validate", {
+        guess: guess,
+      });
+      setGuess("");
+      setWordsPlayed(wordsPlayed + 1);
+      setGameState("result");
     } catch {
-      console.log("error");
+      console.error("Error submitting guess");
     }
-    const response = await axios.get("http://localhost:3000/api/ekreb/score");
-    setScore(response.data.score);
-    
+
+    try {
+      const response = await axios.get("http://localhost:3000/api/ekreb/score");
+      setScore(response.data.score);
+      setCorrect(response.data.correctness);
+    } catch (error) {
+      console.error("Error getting score:", error);
+    }
   };
 
-  useEffect(() => {
-    const fetchWord = async () => {
-      try {
-        const response = await axios.get(
-          "http://localhost:3000/api/ekreb/random-word"
-        );
-        if (response.data) {
-          setWord(response.data.word);
-        }
-      } catch (error) {
-        console.error("Error fetching word:", error);
+  //fetches a new word from the server
+  const fetchWord = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:3000/api/ekreb/random-word"
+      );
+      if (response.data) {
+        setOriginalWord(response.data.originalWord);
+        setScrambledWord(response.data.scrambledWord);
       }
-    };
+    } catch (error) {
+      console.error("Error fetching word:", error);
+    }
+  };
 
-    fetchWord();
-  }, []);
+  //resets game state, score, words played, and resets user score on server
+  const restartGame = async () => {
+    setGameState("game");
+    setScore(0);
+    setWordsPlayed(0);
+    try {
+      await axios.patch("http://localhost:3000/api/ekreb/score");
+    } catch (error) {
+      console.error("Error restarting game:", error);
+    }
+  };
 
-  return (
-    <>
-      <div className="heading">
-        <h1>ekreb</h1>
-        <h2>Guess the scrambled word!</h2>
+  let content;
+  if (gameState === "start") {
+    content = (
+      <div className="start">
+        <h1>Welcome to ekreb</h1>
+        <p>Guess the scrambled word!</p>
+        <button
+          onClick={() => {
+            setGameState("game");
+            fetchWord();
+          }}
+        >
+          Start
+        </button>
       </div>
-
+    );
+  } else if (gameState === "game") {
+    content = (
       <div className="game">
-        <p>Word: {word}</p>
+        <h1>ekreb</h1>
+        <p>Word: {scrambledWord}</p>
         <input
           type="string"
           placeholder="Enter your guess here!"
@@ -58,9 +96,36 @@ function App() {
         />
         <button onClick={handleGuess}>Guess</button>
         <p>Score: {score}</p>
+        <button onClick={fetchWord}>New Word</button>
       </div>
-    </>
-  );
+    );
+  } else if (gameState === "result") {
+    content = (
+      <div className="result">
+        <h2>{correct ? "Correct" : "Wrong"}!</h2>
+        <p>Scrambled Word: {scrambledWord}</p>
+        <p>Original Word: {originalWord}</p>
+        <button
+          onClick={() => {
+            setGameState("game");
+            fetchWord();
+          }}
+        >
+          Next
+        </button>
+      </div>
+    );
+  } else if (gameState === "end") {
+    content = (
+      <div className="end">
+        <h1>Game Over!</h1>
+        <p>Your score was: {score}</p>
+        <button onClick={restartGame}>Restart</button>
+      </div>
+    );
+  }
+
+  return <>{content}</>;
 }
 
 export default App;
