89e9d367d5559ecd1027f7973f99da068e25ad10
Tristan <vantristan04@gmail.com>
2024-11-13 00:20:12 -0600

    readd files

---
+++ client/src/App.jsx
@@ -0,0 +1,167 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+
+function App() {
+  const [guess, setGuess] = useState("");
+  const [originalWord, setOriginalWord] = useState("");
+  const [scrambledWord, setScrambledWord] = useState("");
+  const [score, setScore] = useState(0);
+  const [correct, setCorrect] = useState(false);
+  const [gameState, setGameState] = useState("start");
+  const [wordsPlayed, setWordsPlayed] = useState(0);
+  const [showHint, setShowHint] = useState(false);
+  const [timer, setTimer] = useState(60);
+
+  //ends game after 5 words and controls timer for each word
+  useEffect(() => {
+    // number of rounds
+    if (wordsPlayed === 5) {
+      setGameState("end");
+    }
+
+    // timer
+    if (gameState === "game") {
+      const timerId = setInterval(() => {
+        if (timer > 0) {
+          setTimer((prevTimer) => prevTimer - 1);
+        } else {
+          setGuess("");
+          handleGuess();
+          clearInterval(timerId);
+        }
+      }, 1000);
+
+      return () => clearInterval(timerId);
+    }
+  }, [wordsPlayed, gameState, timer]);
+
+  //sends guess to sever for validation, clears guess, fetches new word, increments words played, sets timer to 60
+  const handleGuess = async () => {
+    try {
+      await axios.post("http://localhost:3000/api/ekreb/validate", {
+        guess: guess,
+      });
+      setGuess("");
+      setGameState("result");
+      setShowHint(false);
+      setTimer(60);
+    } catch {
+      console.error("Error submitting guess");
+    }
+
+    try {
+      const response = await axios.get("http://localhost:3000/api/ekreb/score");
+      setScore(response.data.score);
+      setCorrect(response.data.correctness);
+    } catch (error) {
+      console.error("Error getting score:", error);
+    }
+  };
+
+  //fetches a new word from the server and stores original and scrambled word in states 
+  const fetchWord = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:3000/api/ekreb/random-word"
+      );
+      if (response.data) {
+        setOriginalWord(response.data.originalWord);
+        setScrambledWord(response.data.scrambledWord);
+      }
+    } catch (error) {
+      console.error("Error fetching word:", error);
+    }
+  };
+
+  //resets game state, score, words played, and resets user score on server
+  const restartGame = async () => {
+    setGameState("start");
+    setScore(0);
+    setWordsPlayed(0);
+
+    try {
+      await axios.patch("http://localhost:3000/api/ekreb/score");
+    } catch (error) {
+      console.error("Error restarting game:", error);
+    }
+  };
+
+  //changes state of the hint to true if button is clicked
+  const revealHint = () => {
+    if (originalWord && originalWord.length > 0) {
+      setShowHint(true);
+    }
+  };
+
+  let content;
+  //start screen
+  if (gameState === "start") {
+    content = (
+      <div className="start">
+        <h1>ekreb</h1>
+        <h2>
+          Guess the scrambled word! You have 1 minute to unscramble a word.
+          After 5 words your score will be displayed.
+        </h2>
+
+        <button
+          onClick={() => {
+            setGameState("game");
+            fetchWord();
+          }}
+        >
+          Start
+        </button>
+      </div>
+    );
+    //game screen
+  } else if (gameState === "game") {
+    content = (
+      <div className="game">
+        <h1>Word: {scrambledWord}</h1>
+        <input
+          type="string"
+          placeholder="Enter your guess here!"
+          value={guess}
+          onChange={(e) => setGuess(e.target.value)}
+        />
+        <button id="guess" onClick={handleGuess}>Guess</button>
+        <p>Time Left: {timer}</p>
+        {showHint && <p>Hint: The first letter is {originalWord.charAt(0)}</p>}
+        <button id="hint" onClick={revealHint}>Hint</button>
+      </div>
+    );
+    //result screen
+  } else if (gameState === "result") {
+    content = (
+      <div className="result">
+        <h2>{correct ? "Correct" : "Wrong"}!</h2>
+        <p>Scrambled Word: {scrambledWord}</p>
+        <p>Original Word: {originalWord}</p>
+        <button
+          onClick={() => {
+            setWordsPlayed(wordsPlayed + 1);
+            setGameState("game");
+            fetchWord();
+          }}
+        >
+          Next
+        </button>
+      </div>
+    );
+    //end screen
+  } else if (gameState === "end") {
+    content = (
+      <div className="end">
+        <h1>Game Over!</h1>
+        <p>Your score was: {score}/5</p>
+        <button onClick={restartGame}>Restart</button>
+      </div>
+    );
+  }
+
+  return <>{content}</>;
+}
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const axios = require("axios");
+const app = express();
+const cors = require("cors"); 
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors());
+
+let score = 0;
+let correctness = false;
+let originalWord = "";
+
+//scramble word from api
+function scrambleWord(word) {
+  const characters = word.split("");
+
+  for (let i = characters.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [characters[i], characters[j]] = [characters[j], characters[i]];
+  }
+
+  const scrambledString = characters.join("");
+  return scrambledString;
+}
+//get a word from the api, scramble it, and send original and scrambled word to client
+app.get("/api/ekreb/random-word", async (req, res) => {
+  try {
+    const response = await axios.get(
+      "https://random-word-api.herokuapp.com/word"
+    );
+
+    if (response.data && response.data.length > 0) {
+      originalWord = response.data[0];
+      const scrambledWord = scrambleWord(originalWord);
+      res.status(200).json({ originalWord, scrambledWord });
+    } else {
+      res.status(404).json({ message: "Word not found in API" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+//check user guesses and update score and correctness variables accordingly 
+app.post("/api/ekreb/validate", (req, res) => {
+  const { guess } = req.body;
+
+  if (guess === originalWord) {
+    score += 1;
+    correctness = true;
+    res.status(200).json({score});
+  } else {
+    res.status(200).json({score});
+    correctness = false;
+  }
+});
+
+//get user score and correctness of guess
+app.get("/api/ekreb/score", (req, res) => {
+  res.status(200).json({ score, correctness });
+});
+
+//reset user score and correctness of guess
+app.patch("/api/ekreb/score", (req, res) => {
+  score = 0;
+  correctness = false;
+  res.status(200).json({ score, correctness });
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
